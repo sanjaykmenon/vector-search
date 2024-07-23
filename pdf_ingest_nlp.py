cf07b9a0225f4149a3c99aa607a89805bba6b0bc
Sanjay Krishna <3213319+sanjaykmenon@users.noreply.github.com>
2024-07-23 16:35:33 -0400

    Refactor PDF text extraction and NLP processing functions

--- pdf_ingest_nlp.py
+++ pdf_ingest_nlp.py
@@ -1,10 +1,3 @@
-# do standard NLP to extract data from full text and write into a new supabase table
-
-#do only eb-1 cases
-
-#part 2 of this will be the search py file that uses tantivy to search the data based on user query
-
-
 from datetime import date as dt, datetime
 from openai import OpenAI
 from pydantic import BaseModel, Field, field_validator
@@ -41,9 +34,6 @@ instructor_client = instructor.from_openai(OpenAI())
 #openai client
 openai_client = OpenAI()
 
-# Load the Spacy model
-nlp = spacy.load("en_core_web_sm")
-
 # Function to extract text from a PDF
 def extract_text(pdf_path: str) -> str:
     """
@@ -54,16 +44,38 @@ def extract_text(pdf_path: str) -> str:
     doc.close()
     return "\n".join(full_text)
 
-def process_text(text: str) -> str:
-    """
-    remove punctuation
-    remove whitespace, stop words
-    convert to lowercase
-    tokenizzation??
-    normalization: include stemming and lemmatization
-    handling contractions
-    part of speech tagging
-    Named entity recognition
-    """
+#modify class Reason for all individual criteria and include CFR code and evidence
+#add specific attribute for each criteria
+#add class that extracts job title. 
+# evals/ validation??
 
+class Reason(BaseModel):
+    reason: str = Field(..., description="The individual reason for approval or denial")
+    cfr_code: str = Field(..., description="The respective CFR code")
+    evidence: str = Field(..., description="The evidence provided for the reason")
 
+class DocumentInfo(BaseModel):
+    title: str = Field(..., description="The title of the document with specific details")
+    beneficiary_details: List[str] = Field(..., description="extract details of beneficiary such as where they are from, what did they do and anything else that can be used to uniquely identify them")
+    beneficiary_status: str = Field(..., description="extract details of the type of visa / status")
+    key_reasons: List[Reason] = Field(..., description="extract the individual reasons for approval or denial, the respective CFR codes, and the evidence provided for each reason. ")
+    summary: List[str] = Field(..., description="add details of entites, people, locations and any other specific detail")
+    date_of_application: dt = Field(..., description="extract date of the document")
+    summary_embedding: List[float] = Field(..., description="OpenAI embedding of the summary")
+    footnotes: List[str] = Field(..., description="extract all footnotes mentioned in the text.")
+    cfr_code: List[str] = Field(..., description="Extract all the sections of the U.S. Code and the Code of Federal Regulations mentioned in the text. ")
+
+    summary: str = None
+    full_text: str = None
+
+    def set_full_text(self, full_text: str):
+        self.full_text = full_text
+    
+    def set_summary(self, summary: str):
+        self.summary = summary
+
+    def set_summary_embedding(self, summary_embedding: List[float]):
+        self.summary_embedding = summary_embedding
+
+# Load the Spacy model
+nlp = spacy.load("en_core_web_sm")
